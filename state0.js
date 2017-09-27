ab5fdbb84fba1d05a3849620b1691373e12c295e
Jared Allmaras <jaredallmaras@gmail.com>
2017-09-27 18:01:08 -0500

    fixed zombie animation

--- state0.js
+++ state0.js
@@ -9,8 +9,7 @@ demo.state0.prototype = {
         game.load.image('rock', 'assets/Tilemaps/rock.png');
 
         game.load.spritesheet('hunter', 'assets/sprites/hunter.png');
-        game.load.spritesheet('zombieDown','assets/sprites/zombieSpritesheet.png', 67, 48);
-        game.load.spritesheet('zombieUp', 'assets/sprites/zombieBackSpritesheet.png', 67, 52);
+        game.load.spritesheet('zombie','assets/sprites/zombieSprites.png', 52, 67);
     },
     
     create: function() {
@@ -32,17 +31,24 @@ demo.state0.prototype = {
         player = game.add.sprite(200, 200, 'hunter');
         game.physics.enable(player);
         player.body.collideWorldBounds = true;
+        player.anchor.setTo(0.5, 0.5);
         game.camera.follow(player);
         
         //Create Zombies 
         zombies = game.add.group();
         zombies.enableBody = true;
+        
         //create zombies 
         for ( var i = 0; i<50; i++)
         {
-        var zombie = zombies.create(game.world.randomX,game.world.randomY,'zombie');
+            var zombie = zombies.create(game.world.randomX,game.world.randomY,'zombie');
+            zombie.anchor.setTo(0.5, 0.5);
         }
         
+        zombies.callAll('animations.add', 'animations', 'upLeft', [4, 5, 6, 7], 8, true);
+        zombies.callAll('animations.add', 'animations', 'upRight', [0, 1, 2, 3], 8, true);
+        zombies.callAll('animations.add', 'animations', 'downLeft', [12, 13, 14, 15], 8, true);
+        zombies.callAll('animations.add', 'animations', 'downRight', [8, 9, 10, 11], 8, true);
         
         cursors = game.input.keyboard.createCursorKeys();
     },
@@ -50,6 +56,25 @@ demo.state0.prototype = {
     update: function() {
         
         zombies.forEach(game.physics.arcade.moveToObject, game.physics.arcade, false, player, 100);
+        
+        
+        if(zombies.forEach(function(self) {
+            angle = (Phaser.Math.normalizeAngle(game.physics.arcade.angleBetween(self, player)))
+            
+            if(angle >= 0 && angle <= 1.5708) {
+                self.animations.play('downRight');
+            }
+            if(angle > 1.5708 && angle <= 3.14159) {
+                self.animations.play('downLeft');
+            }
+            if(angle > 3.14159 && angle <= 4.71239) {
+                self.animations.play('upLeft');
+            }
+            if(angle > 4.71239 && angle <= 6.28319) {
+                self.animations.play('upRight');
+            }}
+            , game.physics.arcade, false));
+
         
         if(cursors.up.isDown){
           player.body.velocity.y = -vel;
