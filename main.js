dd2237b5927fd1773a0f0faea33189f5cf276cad
Jared Allmaras <jaredallmaras@gmail.com>
2017-10-15 12:58:45 -0500

    fixed main.js

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 var game = new Phaser.Game(1131,621, Phaser.AUTO);
-=======
-var game = new Phaser.Game(1120,600, Phaser.AUTO);
->>>>>>> ed2e7d9ce03a41c14d9c95a650cae6ce36b6d3a1
 game.state.add('state0', demo.state0);
 game.state.add('state1', demo.state1);
 game.state.start('state1');
